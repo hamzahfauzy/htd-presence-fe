52409b2690e8d77ed639ef7b8c339e0f84b6c39c
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-10-28 14:55:48 +0700

    update dashboard

--- src/pages/dashboard/Index.vue
+++ src/pages/dashboard/Index.vue
@@ -26,26 +26,12 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-12 lg:col-4 xl:col-4">
+		<div class="col-12 lg:col-4 xl:col-4" v-for="worktimeItem in worktimeItems" :key="worktimeItem.id">
 			<div class="card mb-0">
 				<div class="flex justify-content-between mb-3">
 					<div>
-						<span class="block text-500 font-medium mb-3">Absen Masuk Hari Ini</span>
-						<div class="text-900 font-medium text-xl">{{ins}}</div>
-					</div>
-					<div class="flex align-items-center justify-content-center bg-orange-100 border-round"
-						style="width:2.5rem;height:2.5rem">
-						<i class="pi pi-clock text-orange-500 text-xl"></i>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-12 lg:col-4 xl:col-4">
-			<div class="card mb-0">
-				<div class="flex justify-content-between mb-3">
-					<div>
-						<span class="block text-500 font-medium mb-3">Absen Pulang Hari Ini</span>
-						<div class="text-900 font-medium text-xl">{{outs}}</div>
+						<span class="block text-500 font-medium mb-3">Presensi {{worktimeItem.name}} Hari Ini</span>
+						<div class="text-900 font-medium text-xl">{{worktimeItem.counter}}</div>
 					</div>
 					<div class="flex align-items-center justify-content-center bg-orange-100 border-round"
 						style="width:2.5rem;height:2.5rem">
@@ -62,17 +48,21 @@ export default {
         return {
             employees:0,
 			workunits:0,
-			ins:0,
-			outs:0,
+			worktimeItems:[]
         }
     },
     async created() {
-        var request = await fetch(process.env.VUE_APP_API_URL+'dashboards/index')
+		var params = ''
+		var userData = JSON.parse(localStorage.getItem("presence_user_data"))
+		if(userData.workunit_id)
+		{
+			params = '?workunit_id='+userData.workunit_id
+		}
+        var request = await fetch(process.env.VUE_APP_API_URL+'dashboards/index'+params)
 		var response = await request.json()
 		this.employees = response.data.employees
 		this.workunits = response.data.workunits
-		this.ins = response.data.ins
-		this.outs = response.data.outs
+		this.worktimeItems = response.data.worktimeItems
     }
 }
 </script>
