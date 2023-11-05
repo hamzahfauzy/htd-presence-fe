f4333ca09271d8a3d4a7c2fdee5a517867a066d0
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-11-06 01:06:25 +0700

    add report request

---
+++ src/pages/report-requests/Index.vue
@@ -0,0 +1,248 @@
+<template>
+    <div class="grid">
+        <div class="col-12">
+            <div class="card">
+                <Toolbar class="mb-4">
+                    <template v-slot:start>
+                        <div class="my-2 d-flex">
+
+                            <Calendar dateFormat="yy-mm-dd" :showIcon="true" :showButtonBar="true" v-model="report.start_at"
+                                class="m-2" placeholder="Pilih Tanggal Awal" @date-select="onDateChange" />
+                            <Calendar dateFormat="yy-mm-dd" :showIcon="true" :showButtonBar="true" v-model="report.end_at"
+                                class="m-2" placeholder="Pilih Tanggal Akhir" />
+
+                            <Dropdown v-model="report.workunit_id" :options="workunits" optionLabel="name"
+                                v-if="['superuser','adminsistem','adminkepegawaian'].includes(role)"
+                                optionValue="id" class="m-2" placeholder="Pilih OPD" :filter="true" />
+                            
+                            <Dropdown v-model="report.report_type" :options="[{name:'Rekapitulasi',value:'rekapitulasi'},{name:'Detail',value:'detail'}]" optionLabel="name"
+                                v-if="['superuser','adminsistem','adminkepegawaian'].includes(role)"
+                                optionValue="value" class="m-2" placeholder="Pilih Report Type" :filter="true" />
+
+                            <Button label="Filter" icon="pi pi-search" class="p-button-success m-2" @click="saveReport()" />
+                        </div>
+                    </template>
+                </Toolbar>
+                <DataTable :value="employees" :lazy="true" v-model:filters="filters"
+                    ref="dt" dataKey="id" :loading="loading" :globalFilterFields="['name']"
+                    v-model:selection="selectedCustomers" :selectAll="selectAll" @select-all-change="onSelectAllChange"
+                    @row-select="onRowSelect" @row-unselect="onRowUnselect"
+                    paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                    :rowsPerPageOptions="[5,10,25]"
+                    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+                    responsiveLayout="scroll">
+                    <Column field="id" header="ID" :sortable="true">
+                        <template #body="slotProps">
+                            <span class="p-column-title">ID</span>
+                            {{slotProps.data.id}}
+                        </template>
+                    </Column>
+                    <Column field="workunit.name" header="OPD"></Column>
+                    <Column field="start_at" header="Tanggal Awal"></Column>
+                    <Column field="end_at" header="Tanggal Akhir"></Column>
+                    <Column field="status" header="Status"></Column>
+                    <Column field="file_url" header="File URL"></Column>
+                </DataTable>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {FilterMatchMode} from 'primevue/api';
+import WorkunitService from '../../service/WorkunitService';
+import ReportRequestService from '../../service/ReportRequestService';
+export default {
+    data() {
+        return {
+            loading: false,
+            reports:[],
+            onsearchtimeout: null,
+            totalRecords: 0,
+            selectAll: false,
+            filters: {},
+            lazyParams: {},
+            date_start:null,
+            date_end:null,
+            cdate_start:null,
+            cdate_end:null,
+            report: {
+                report_type:null,
+                start_at:null,
+                end_at:null,
+                workunit_id:null,
+            },
+            workunits:[],
+            selectedWorkunit:{},
+            role:localStorage.getItem("presence_app_role"),
+            fields:{
+                'OPD':'workunit.name',
+                'Tanggal Awal':'start_at',
+                'Tanggal Akhir':'end_at',
+                'Status':'status',
+                'File URL':'file_url',
+            }
+        }
+    },
+    workunitService: null,
+    reportService: null,
+    created() {
+        this.workunitService = new WorkunitService();
+        this.reportService = new ReportRequestService();
+        this.initFilters()
+    },
+    mounted() {
+        this.loading = true;
+
+        this.lazyParams = {
+            first: 0,
+            page: 0,
+            rows: this.$refs.dt.rows,
+            sortField: null,
+            sortOrder: 1,
+            date_start:null,
+            date_end:null,
+            filters: this.filters
+        };
+
+        this.loadLazyData();
+    },
+    methods: {
+        loadLazyData() {
+            this.loading = true;
+
+            var userData = JSON.parse(localStorage.getItem("presence_user_data"))
+            if(userData.workunit_id) this.selectedWorkunit.id = userData.workunit_id
+
+            setTimeout(() => {
+
+                this.workunitService.getWorkunits()
+                    .then(data => {
+                        if ('redirectTo' in data) {
+                            localStorage.removeItem('presence_app_token')
+                            localStorage.removeItem('presence_app_role')
+                            this.$router.push(data.redirectTo)
+                        }
+                        this.workunits = data.data;
+                        this.loading = false;
+                    }
+                    );
+
+                if(this.selectedWorkunit.id){
+                    this.reportService.getReports(this.selectedWorkunit.id)
+                        .then(data => {
+                            if ('redirectTo' in data) {
+                                localStorage.removeItem('presence_app_token')
+                                localStorage.removeItem('presence_app_role')
+                                this.$router.push(data.redirectTo)
+                            }
+                            this.reports = data.data;
+                            this.totalRecords = data.total;
+                            this.loading = false;
+                        }
+                        );
+                }
+            }, Math.random() * 1000 + 250);
+        },
+        onPage(event) {
+            this.lazyParams = event;
+            this.loadLazyData();
+        },
+        onSort(event) {
+            var page = this.lazyParams.page
+            this.lazyParams = event;
+            this.lazyParams.page = page
+            this.loadLazyData();
+        },
+        onFilter() {
+            if(this.onsearchtimeout)
+                clearTimeout(this.onsearchtimeout)
+            this.onsearchtimeout = setTimeout( () => {
+                this.lazyParams.filters = this.filters;
+                this.loadLazyData();
+            }, 1000)
+        },
+
+        onDownloadPdf() {
+            window.open(this.employeeService.getEmployeesReportPdfLink(this.lazyParams, this.selectedWorkunit.id ?? this.userData.workunit_id))
+        },
+
+        onDateChange(){
+            if (this.date_start) {
+                let d = this.date_start
+                let day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate()
+                let month = (d.getMonth() + 1) < 10 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1)
+                this.cdate_start = d.getFullYear() + "-" + month + "-" + day
+            }
+
+            if (this.date_end) {
+                let d = this.date_end
+                let day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate()
+                let month = (d.getMonth() + 1) < 10 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1)
+                this.cdate_end = d.getFullYear() + "-" + month + "-" + day
+            }
+        },
+
+        onSearch(){
+            this.lazyParams.filters = this.filters;
+
+            if (this.date_start) {
+                let d = this.date_start
+                let day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate()
+                let month = (d.getMonth() + 1) < 10 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1)
+                this.lazyParams.date_start = d.getFullYear() + "-" + month + "-" + day
+            }
+
+            if (this.date_end) {
+                let d = this.date_end
+                let day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate()
+                let month = (d.getMonth() + 1) < 10 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1)
+                this.lazyParams.date_end = d.getFullYear() + "-" + month + "-" + day
+            }
+
+            this.loadLazyData();
+        },
+        
+        onPengajuanWorkunitChange() {
+            this.workunitService.getWorkunit(this.pengajuan.workunit_id)
+            .then(data => {
+                if ('redirectTo' in data) {
+                    localStorage.removeItem('presence_app_token')
+                    localStorage.removeItem('presence_app_role')
+                    this.$router.push(data.redirectTo)
+                }
+                this.employees = data.employees;
+            });
+        },
+		initFilters() {
+            this.filters = {
+                'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
+            }
+        },
+        saveReport() {
+			this.reportService.createReport(this.paidLeave)
+            .then(res => {
+                if(!res.success)
+                {
+                    this.$swal({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: res.message,
+                    })
+                }
+                else
+                {
+                    this.loadLazyData()
+                    this.paidLeaveDialog = false;
+                    this.paidLeave = {};
+                    this.$swal({
+                        icon: 'success',
+                        title: 'Success',
+                        text: res.message
+                    })
+                }
+            })
+		},
+    }
+}
+</script>
---
+++ src/service/ReportRequestService.js
@@ -0,0 +1,55 @@
+export default class ReportRequestService {
+
+	getReports(workunit_id = false) {
+        // const queryParams = params ? Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&') : '';
+
+        var userData = JSON.parse(localStorage.getItem('presence_user_data'))
+        if(userData.workunit_id)
+        {
+            params += '&workunit_id='+userData.workunit_id;
+        }
+
+        if(workunit_id)
+        {
+            params += '&workunit_id='+workunit_id;
+        }
+        
+		return fetch(process.env.VUE_APP_API_URL+'report-requests?' + params,{
+            headers:{
+                'authorization' : 'Bearer '+localStorage.getItem('presence_app_token')
+            }
+        })
+        .then(res => {
+            if(res.status == 401)
+            {
+                return {data:{redirectTo:"login"}};
+            }
+            return res.json()
+        })
+        .then(d => d.data);
+    }
+
+    createReportRequest(data){
+        return fetch(process.env.VUE_APP_API_URL+'report-requests/',{
+            method:'POST',
+            headers:{
+                'authorization' : 'Bearer '+localStorage.getItem('presence_app_token'),
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body:JSON.stringify({
+                workunit_id:data.workunit_id,
+                start_at:data.start_at,
+                end_at:data.end_at,
+                report_type:data.report_type,
+            })
+        })
+        .then(res => {
+            if(res.status == 401)
+            {
+                return {redirectTo:"Login"};
+            }
+            return res.json()
+        })
+    }
+	
+}
